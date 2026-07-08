bfdc3aa199e238de22b0fb13f0025fd987e0e69b
hilmazein <alfinahilmazein@gmail.com>
2026-07-08 13:39:02 +0700

    update

--- frontend/src/components/ui/textarea.jsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import { cn } from "@/lib/utils";
-
-const Textarea = React.forwardRef(({ className, ...props }, ref) => {});
-Textarea.displayName = "Textarea";
-export { Textarea };
